da83d0c13bacf4c20fa40d4d1785f82770d93d2b
FatimahAlsaigh <toome1995@hotmail.com>
2019-05-02 12:46:18 +0300

    Fatimah finalProjec

--- main.js
+++ main.js
@@ -5,16 +5,16 @@ $(document).ready(function () {
 
     $.ajax({
         method: 'GET',
-        url: 'https://api.foursquare.com/v2/venues/search?near=Riyadh&categoryId=4bf58dd8d48988d14e941735&client_id=P11JFTNLEHOJSOG5YRKKJY4IXA21F2ANWXKGK1IS2V0YQBQF&client_secret=DPRMYDP1BNU22LWRM05DZZYVG0BOTJCBSPC01P0JE4VAQTH0&v=20190429',
+        url: 'https://api.foursquare.com/v2/venues/search?near=Riyadh&categoryId=4bf58dd8d48988d14e941735&client_id=clientID&client_secret=SecretKey&v=20190429',
         success: function (data) {
             console.log('first call');
             var venues = data.response.venues
             console.log(data);
 
-            for (let i = 0; i < 10; i++) {
+            for (let i = 0; i < 20; i++) {
                 $.ajax({
                     method: 'GET',
-                    url: 'https://api.foursquare.com/v2/venues/' + venues[i].id + '/tips?client_id=P11JFTNLEHOJSOG5YRKKJY4IXA21F2ANWXKGK1IS2V0YQBQF&client_secret=DPRMYDP1BNU22LWRM05DZZYVG0BOTJCBSPC01P0JE4VAQTH0&v=20190429&v=20190429',
+                    url: 'https://api.foursquare.com/v2/venues/' + venues[i].id + '/tips?client_id=ClientId&client_secret=SecretKey&v=20190429',
                     success: function (result) {
                         console.log(venues[0].id)
                          var $div = $('<div/>').addClass('tile');
